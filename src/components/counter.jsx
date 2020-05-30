0ebf9f411339d8a2b055c493ad51963c35ebe75e
‘peter-robert-cole’ <petercole3@gmail.com>
2020-05-29 18:47:12 -0700

    refactored counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,20 +5,19 @@ export default class Counter extends Component {
 		count: 0
 	};
 
-	styles = {
-		fontSize: 40,
-		fontWeight: 'bold'
-	};
-
 	render() {
 		return (
 			<React.Fragment>
-				<span style={this.styles} className="badge badge-primary m-3">
-					{this.formatCount()}
-				</span>
+				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button className="btn btn-secondary btn-sm">Increment</button>
 			</React.Fragment>
 		);
+	}
+
+	getBadgeClasses() {
+		let classes = 'badge m-2 badge-';
+		classes += this.state.count === 0 ? 'warning' : 'primary';
+		return classes;
 	}
 
 	formatCount() {
