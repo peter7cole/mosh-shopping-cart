b03d67e011a09a6de9b3a961853bc090c976f6db
‘peter-robert-cole’ <petercole3@gmail.com>
2020-05-31 01:15:56 -0700

    destructured objects to remove this.props redundancy

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class NavBar extends Component {
-	render() {
-		return (
-			<div>
-				<nav className="navbar navbar-light bg-light">
-					<a className="navbar-brand" href="/">
-						Cart{' '}
-						<span className="badge badge-pill badge-secondary">
-							{this.props.totalCounters}
-						</span>
-					</a>
-				</nav>
-			</div>
-		);
-	}
-}
+// Stateless Functional Component
+
+const NavBar = ({ totalCounters }) => {
+	return (
+		<div>
+			<nav className="navbar navbar-light bg-light">
+				<a className="navbar-brand" href="/">
+					Cart{' '}
+					<span className="badge badge-pill badge-secondary">
+						{totalCounters}
+					</span>
+				</a>
+			</nav>
+		</div>
+	);
+};
+
+export default NavBar;
