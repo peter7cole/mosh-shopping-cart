d40d70423986dced56233d278b911774550dc63f
‘peter-robert-cole’ <petercole3@gmail.com>
2020-05-31 00:49:14 -0700

    added NavBar component skeleton

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.scss';
+import NavBar from '../NavBar';
 import CounterList from '../CounterList';
 
 export default class App extends Component {
 	render() {
 		return (
-			<div>
-				<CounterList />
-			</div>
+			<React.Fragment>
+				<NavBar />
+				<main className="container">
+					<CounterList />
+				</main>
+			</React.Fragment>
 		);
 	}
 }
